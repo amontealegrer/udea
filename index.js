fa4a64f69d2c67818d8257f9909d365d4d130a9d
amontealegrer <amontealegrer@gmail.com>
2021-11-26 17:44:47 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -3,25 +3,31 @@
 require('dotenv').config()
 const { makeExecutableSchema } = require('graphql-tools')
 const express = require('express')
+const cors = require ('cors')
 const { graphqlHTTP } = require('express-graphql')
 const { readFileSync } = require('fs')
 const { join } = require('path')
-const resolvers = require('./lib/resolvers')
+const resolvers = require ('./lib/resolvers')
 
 const app = express()
 const port = process.env.port || 3000
+const isDev = process.env.NODE_ENV !== 'production'
 
 const typeDefs = readFileSync(
-    join(__dirname, 'lib', 'schema.graphql'),
-    'utf-8'
-)
+        join(__dirname, 'lib', 'schema.graphql'),
+        'utf-8'
+    )
 
-const schema = makeExecutableSchema({typeDefs, resolvers})
+const schema = makeExecutableSchema({
+    typeDefs, resolvers
+})
+
+app.use(cors())
 
 app.use('/api', graphqlHTTP({
     schema: schema,
     rootValue: resolvers,
-    graphiql: true
+    graphiql: isDev
 }))
 
 app.listen(port, () => {
